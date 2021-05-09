1be5d8d4c40b4105ee94bc296f43ad307c0cf7eb
AlissonSantos27 <alissonsantos0692@gmail.com>
2021-05-09 09:58:07 -0400

    finalizando as classes de dominio

--- src/main/java/com/alisson/bookstore/domain/Categoria.java
+++ src/main/java/com/alisson/bookstore/domain/Categoria.java
@@ -1,14 +1,28 @@
 package com.alisson.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categoria {
-	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
-	
+
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
@@ -79,9 +93,5 @@ public class Categoria {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
 
 }
